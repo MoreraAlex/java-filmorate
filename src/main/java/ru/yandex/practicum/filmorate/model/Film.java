6e7097bf7ff6742c02a1d6d68bacf07335071be0
MoreraAlex <136644013+MoreraAlex@users.noreply.github.com>
2026-03-30 19:56:12 +0300

    first commit (#1)
    * first commit
    
    * second commit
    Исправления по комментариям ревьюера:
    - Убраны лишние объявления исключения из FilmController
    - Добавлено логирование и новый метод в ErrorHandler
    
    ---------
    
    Co-authored-by: Морера Боррото Александер Арнальдович <aamoreraborroto@gid.ru>

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-/**
- * Film.
- */
-@Getter
-@Setter
+import java.time.LocalDate;
+
+
+@Data
 public class Film {
+    private final int maxLengthDescription = 200;
+    private Long id;
+
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+    @Size(max = maxLengthDescription, message = "Описание не должно превышать " + maxLengthDescription + " символов")
+    private String description;
+
+    @NotNull
+    private LocalDate releaseDate;
+
+    @Positive(message = "Продолжительность фильма должна быть положительным числом")
+    private int duration;
 }
